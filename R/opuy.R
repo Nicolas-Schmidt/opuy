be8b944c8f62b7568ef6bdbabf8c13c85cb39ee0
Nicolas-Schmidt <nicoschlab@gmail.com>
2020-04-14 13:41:54 -0300

    opuy description

--- R/opuy.R
+++ R/opuy.R
@@ -6,25 +6,25 @@
 #' de las empresas consultoras que realizan las encuestas.
 #'
 #'\describe{
-#'    \item{medicion}{}
-#'    \item{empresa}{}
-#'    \item{tipo_eleccion}{}
-#'    \item{anio_medicion}{}
-#'    \item{anio_gobierno}{}
-#'    \item{eleccion}{}
-#'    \item{fecha}{}
-#'    \item{partido}{}
-#'    \item{sigla}{}
-#'    \item{candidato}{}
-#'    \item{presidente}{}
-#'    \item{categoria}{}
-#'    \item{categoria_unificada}{}
-#'    \item{valor}{}
+#'    \item{medicion}{Indicador disponible. Hasta el momento solo hay dos: Intencio de voto y Evaluacion de la gestion del Presidente.}
+#'    \item{empresa}{Nombre de la empresa consultora que hizo la medicion.}
+#'    \item{tipo_eleccion}{Tipo de eleccion: Nacional, Balotaje o Internas. Esta variables es util para el indicador 'Intencion de voto'.}
+#'    \item{anio_medicion}{Anio en el que se publico la medicion.}
+#'    \item{anio_gobierno}{Anio de gobierno. Esta variables es un numero entro que va desde el 1 hasta el 5 indicado respectivamente el anio de gobierno. Es particularmente util para el indicador 'Evaluacion de la gestion del Presidente'. Con esta variable es mas simple realizar comparaciones entre Presidentes.}
+#'    \item{eleccion}{Esta variable refiere al anio de la eleccion a la que está asociada la medicion. Por ejemplo, se puede tener una medicion de Intencion de Voto del anio 2017, y esta corresponde a la eleccion del 2019.}
+#'    \item{fecha}{Fecha exacta de la publicacion de la medidion.}
+#'    \item{partido}{Nombre del partido politico para la medicion de Intencion de voto.}
+#'    \item{sigla}{Sigla del partido politico.}
+#'    \item{candidato}{Nombre del candidato para el caso de la Intencion de voto en elecciones internas.}
+#'    \item{presidente}{Nombre del Presidente para la medicion Evaluacion de la gestion del presidente.}
+#'    \item{categoria}{Categoria original de la medicion para el caso de Evaluacion de la gestion del presidente. Esta variable se mide comunmente en escala de Likert.}
+#'    \item{categoria_unificada}{Se armoniza la variable 'categoria' para poder comparar entre empresas consultoras.}
+#'    \item{valor}{Valor del indicador en porcentaje.}
 #'
 #' }
 #' @docType data
 #' @keywords datasets
-#' @name op_uy
+#' @name opuy
 #' @usage data(opuy)
 #' @format data.frame con 4788 filas y 14 columnas
 'opuy'
